a0db1d971fa0b92775d1444b95832807dfdbc776
Vitalii Slobodoniuk <slvitalik24@ukr.net>
2021-12-11 15:25:32 +0200

    task done

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-const time = document.querySelector('p');
-let isFullFormat = true;
+class Clock {
+  constructor(el) {
+    this.el = el;
+    this.el.addEventListener('click', () => this.toggleFormat());
+    this.isFullFormat = true;
+  }
 
-let currentTime = function () {
-  let now = new Date();
-  let hours = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
-  let minutes = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
-  let seconds = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds();
-  return isFullFormat ? `${hours} : ${minutes} : ${seconds}` : `${hours} : ${minutes}`;
-};
+  render() {
+    this.now = new Date();
+    this.hours = this.correctTime(this.now.getHours());
+    this.minutes = this.correctTime(this.now.getMinutes());
+    this.seconds = this.correctTime(this.now.getSeconds());
+    this.el.innerHTML = this.isFullFormat
+      ? `${this.hours} : ${this.minutes} : ${this.seconds}`
+      : `${this.hours} : ${this.minutes}`;
+  }
 
-time.addEventListener('click', function () {
-  isFullFormat = !isFullFormat;
-});
+  toggleFormat() {
+    this.isFullFormat = !this.isFullFormat;
+  }
 
-setInterval(() => {
-  time.innerText = currentTime();
-}, 250);
+  correctTime(value) {
+    return value < 10 ? '0' + value : value;
+  }
+}
+
+let time = document.getElementById('clock');
+let clock = new Clock(time);
+setInterval(() => clock.render(), 250);
+
+// вторые часы
+// let time1 = document.getElementById('clock1');
+// let clock1 = new Clock(time1);
+// setInterval(() => clock1.render(), 250);
